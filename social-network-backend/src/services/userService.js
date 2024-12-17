d4d968b2a3b8cf398b8e5d2016e3ee5df82fffeb
Alessandro B <ale_alessandro_ale@hotmail.com>
2024-12-17 19:23:02 -0300

    create function getUserById in userService

--- social-network-backend/src/services/userService.js
+++ social-network-backend/src/services/userService.js
@@ -4,3 +4,15 @@ async function getAllUsers() {
   const result = await User.find({});
   return result;
 }
+
+async function getUserById(userId) {
+  const [user] = await User.find({
+    where: { id: userId },
+  });
+
+  if (!user) {
+    throw new Error('User with id ' + userId + ' not found');
+  }
+
+  return user;
+}
